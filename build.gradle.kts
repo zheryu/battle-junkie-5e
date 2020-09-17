61f5d5d7c14918043ef09a3521849da369ea592e
zheryu <eric.zhou.us@gmail.com>
2020-09-17 16:20:40 -0700

    migrate gradle to kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 plugins {
     // Apply the java plugin to add support for Java
-    id 'java'
+    java
 
     // Apply the application plugin to add support for building a CLI application.
-    id 'application'
+    application
 
     // Apply support for JavaFX
-    id 'org.openjfx.javafxplugin' version '0.0.8'
+    id("org.openjfx.javafxplugin") version "0.0.8"
 }
 
 repositories {
@@ -17,26 +17,26 @@ repositories {
 
 dependencies {
     // This dependency is used by the application.
-    implementation 'com.google.guava:guava:29.0-jre'
+    implementation("com.google.guava:guava:29.0-jre")
 
     // Use JUnit Jupiter API for testing.
-    testImplementation 'org.junit.jupiter:junit-jupiter-api:5.6.2'
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.2")
 
     // Use JUnit Jupiter Engine for testing.
-    testRuntimeOnly 'org.junit.jupiter:junit-jupiter-engine:5.6.2'
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.2")
 }
 
 application {
     // Define the main class for the application.
-    mainClassName = 'App'
+    mainClassName = "App"
 }
 
-test {
+tasks.test {
     // Use junit platform for unit tests
     useJUnitPlatform()
 }
 
 javafx {
     version = "14"
-    modules = ['javafx.controls', 'javafx.fxml']
+    modules = listOf("javafx.controls", "javafx.fxml")
 }
